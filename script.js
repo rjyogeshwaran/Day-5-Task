936391cb35e8b20764fb17c0f04f53aa458d722b
“rjyogeshwarangit config --global user.email “rjyogeshwaran@gmail.com <rjyogeshwaran@gmail.com>
2023-10-31 22:00:06 +0530

    call back hell

--- script.js
+++ script.js
@@ -16,56 +16,48 @@ all.append(count,countdown)
 setTimeout(() => {
     console.log("10")
     countdown.innerText = '10'
+    setTimeout(() => {
+        console.log("9")
+        countdown.innerText = '9'
+        setTimeout(() => {
+            console.log("8")
+            countdown.innerText = '8'
+            setTimeout(() => {
+                console.log("7")
+                countdown.innerText = '7'
+                setTimeout(() => {
+                    console.log("6")
+                    countdown.innerText = '6'
+                    setTimeout(() => {
+                        console.log("5")
+                        countdown.innerText = '5'
+                        setTimeout(() => {
+                            console.log("4")
+                            countdown.innerText = '4'
+                            setTimeout(() => {
+                                console.log("3")
+                                countdown.innerText = '3'
+                                setTimeout(() => {
+                                    console.log("2")
+                                    countdown.innerText = '2'
+                                    setTimeout(() => {
+                                        console.log("1")
+                                        countdown.innerText = '1'
+                                        setTimeout(() => {
+                                            console.log("Happy Independence Day")
+                                            countdown.innerText = ''
+                                            count.innerText = 'Happy Independence Day'
+                                            count.className = 'style'
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
 }, 1000);
-setTimeout(() => {
-    console.log("9")
-    countdown.innerText = '9'
-}, 2000);
-setTimeout(() => {
-    console.log("8")
-    countdown.innerText = '8'
-}, 3000);
-
-setTimeout(() => {
-    console.log("7")
-    countdown.innerText ='7'
-}, 4000);
-
-setTimeout(() => {
-    console.log("6")
-    countdown.innerText= '6'
-}, 5000);
-
-setTimeout(() => {
-    console.log("5")
-    countdown.innerText='5'
-}, 6000);
-
-setTimeout(() => {
-    console.log("4")
-    countdown.innerText ='4'
-}, 7000);
-
-setTimeout(() => {
-    console.log("3")
-    countdown.innerText = '3'
-}, 8000);
-
-setTimeout(() => {
-    console.log("2")
-    countdown.innerText= '2'
-}, 9000);
-
-setTimeout(() => {
-    console.log("1")
-    countdown.innerText = '1'
-}, 10000);
-
-setTimeout(() => {
-    console.log("Happy Independence Day")
-    countdown.innerText = ''
-    count.innerText = 'Happy Independence Day'
-    count.className = 'style'
-}, 11000);
 
 document.body.append(all)
